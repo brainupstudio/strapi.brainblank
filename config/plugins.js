0f2a3e200c7b9210c04d62b37e8e1e62488fb2d0
Luca Pagot <original_jpag@yahoo.it>
2022-10-27 21:07:45 +0200

    Add SEO & ColorPicker plugin ++

--- config/plugins.js
+++ config/plugins.js
@@ -8,9 +8,9 @@
 module.exports = () => {
   return {
     ckeditor: {
-     enabled: true,
-     config:{
-        plugin:{
+      enabled: true,
+      config: {
+        plugin: {
           // disable data-theme tag setting //
           // setAttribute:false,
 
@@ -28,7 +28,7 @@ module.exports = () => {
           // }
           // `
         },
-        editor:{ // editor default config
+        editor: { // editor default config
 
           // https://ckeditor.com/docs/ckeditor5/latest/features/markdown.html
           // if you need markdown support and output set: removePlugins: [''],
@@ -86,15 +86,15 @@ module.exports = () => {
           // https://ckeditor.com/docs/ckeditor5/latest/features/font.html
           fontSize: {
             options: [
-                9,
-                11,
-                13,
-                'default',
-                17,
-                19,
-                21,
-                27,
-                35,
+              9,
+              11,
+              13,
+              'default',
+              17,
+              19,
+              21,
+              27,
+              35,
             ],
             supportAllValues: false
           },
@@ -126,26 +126,26 @@ module.exports = () => {
           // https://ckeditor.com/docs/ckeditor5/latest/features/images/images-overview.html
           image: {
             resizeUnit: "%",
-            resizeOptions: [ {
+            resizeOptions: [{
               name: 'resizeImage:original',
               value: null,
               icon: 'original'
             },
-            {
-              name: 'resizeImage:25',
-              value: '25',
-              icon: 'small'
-            },
-            {
-              name: 'resizeImage:50',
-              value: '50',
-              icon: 'medium'
-            },
-            {
-              name: 'resizeImage:75',
-              value: '75',
-              icon: 'large'
-            } ],
+              {
+                name: 'resizeImage:25',
+                value: '25',
+                icon: 'small'
+              },
+              {
+                name: 'resizeImage:50',
+                value: '50',
+                icon: 'medium'
+              },
+              {
+                name: 'resizeImage:75',
+                value: '75',
+                icon: 'large'
+              }],
             toolbar: [
               'toggleImageCaption',
               'imageTextAlternative',
@@ -170,28 +170,31 @@ module.exports = () => {
           // https://ckeditor.com/docs/ckeditor5/latest/features/headings.html
           heading: {
             options: [
-              { model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph' },
-              { model: 'heading1', view: 'h1', title: 'Heading 1', class: 'ck-heading_heading1' },
-              { model: 'heading2', view: 'h2', title: 'Heading 2', class: 'ck-heading_heading2' },
-              { model: 'heading3', view: 'h3', title: 'Heading 3', class: 'ck-heading_heading3' },
-              { model: 'heading4', view: 'h4', title: 'Heading 4', class: 'ck-heading_heading4' },
+              {model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph'},
+              {model: 'heading1', view: 'h1', title: 'Heading 1', class: 'ck-heading_heading1'},
+              {model: 'heading2', view: 'h2', title: 'Heading 2', class: 'ck-heading_heading2'},
+              {model: 'heading3', view: 'h3', title: 'Heading 3', class: 'ck-heading_heading3'},
+              {model: 'heading4', view: 'h4', title: 'Heading 4', class: 'ck-heading_heading4'},
             ]
           },
           // https://ckeditor.com/docs/ckeditor5/latest/features/general-html-support.html
           // Regular expressions (/.*/  /^(p|h[2-4])$/' etc) for htmlSupport does not allowed in this config
           htmlSupport: {
             allow: [
-                {
-                  name: 'img',
-                  attributes: {
-                      sizes:true,
-                      loading:true,
-                  }
-                },
+              {
+                name: 'img',
+                attributes: {
+                  sizes: true,
+                  loading: true,
+                }
+              },
             ]
           },
-        }
+        },
       }
+    },
+    seo: {
+      enabled: true,
     },
     'import-export-entries': {
       enabled: true,
